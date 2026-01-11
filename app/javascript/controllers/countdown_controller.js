ef124138eb81bd67977f0517766923e75aaef697
tenet64 <keigo.ouchida@gmail.com>
2026-01-12 00:16:05 +0900

    style:controller

--- app/javascript/controllers/countdown_controller.js
+++ app/javascript/controllers/countdown_controller.js
@@ -26,7 +26,7 @@ export default class extends Controller {
 
     const days = Math.floor(diff / 86400) 
     diff %= 86400
-    const hours = Math.floor(diff / 3600)
+    const hours = Math.floor(diff / 3600
     diff %= 3600
     const minutes = Math.floor(diff / 60)
     const seconds = diff % 60
