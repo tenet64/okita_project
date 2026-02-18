3521635a43850e13677a0edcb5b41033bb3d1a24
tenet64 <keigo.ouchida@gmail.com>
2026-02-18 20:44:41 +0900

    xシェアボタンの追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,28 @@
 module ApplicationHelper
+  def default_meta_tags
+    {
+      site: "okita!!",
+      title: "一緒に起床を共有できるサービス",
+      reverse: true,
+      charset: "utf-8",
+      description: "okita!!では、ユーザー同士が一緒に起床することで二度寝を防ぐためのアプリです",
+      keywords: "起床,早起き,サービス",
+      canonical: "https://okita-project.onrender.com/",
+      separator: "|",
+      og: {
+        site_name: :site,
+        title: :title,
+        description: :description,
+        type: "website",
+        url: "https://okita-project.onrender.com/",
+        image: image_url("static_ogp.jpg"),
+        local: "ja-JP"
+      },
+      twitter: {
+        card: "summary_large_image",
+        site: "@obvyamdrss",
+        image: image_url("static_ogp.jpg")
+      }
+    }
+  end
 end
